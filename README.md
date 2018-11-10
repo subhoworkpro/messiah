a0bf73cb71651d04eb369ea5e7fe73e04b7a501b
Subhashish Dutta <sdutta@uplandsoftware.com>
2018-11-10 15:08:45 +0530

    Updates DB

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ db.createUser(
     roles: [ "dbAdmin","userAdmin","dbOwner","read","readWrite" ]
   }
 )
+
+db.changeUserPassword("messiah", "India123")
--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
 	'admintoken' : 'U2FsdGVkX19v0jsoa/94adkW34+Ro8j/aTnHIBhO6U8=',
     'key' : 'GhPMvymzAN',
     'secret': 'intellirioconsultanylabs',
-    'database': 'mongodb://messiah:India@123@localhost:27017/messiah'
+    'database': 'mongodb://messiah:India123@localhost:27017/messiah'
 };
