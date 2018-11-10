4f245b45d964a56900abccfb7397d51d503274a3
Subhashish Dutta <sdutta@uplandsoftware.com>
2018-11-10 15:01:48 +0530

    Updates DB

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-'admintoken' : 'U2FsdGVkX19v0jsoa/94adkW34+Ro8j/aTnHIBhO6U8=',
-   'key' : 'GhPMvymzAN',
-   'secret': 'intellirioconsultanylabs',
-   'database': 'mongodb://admin:admin@intelliriolabs-shard-00-00-fsnjl.mongodb.net:27017,intelliriolabs-shard-00-01-fsnjl.mongodb.net:27017,intelliriolabs-shard-00-02-fsnjl.mongodb.net:27017/test?ssl=true&replicaSet=intelliriolabs-shard-0&authSource=admin'
+	'admintoken' : 'U2FsdGVkX19v0jsoa/94adkW34+Ro8j/aTnHIBhO6U8=',
+    'key' : 'GhPMvymzAN',
+    'secret': 'intellirioconsultanylabs',
+    'database': 'mongodb://messiah:India@123@localhost:27017/messiah'
 };
--- server.js
+++ server.js
@@ -11,16 +11,17 @@ var apiRoutes = express.Router();
 
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
-// var mongoose    = require('mongoose');
-// mongoose.Promise = global.Promise
+var mongoose    = require('mongoose');
+mongoose.Promise = global.Promise
 var CryptoJS = require("crypto-js");
 
 var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
 // var User   = require('./api/models/user'); // get our mongoose model
+var cors = require("cors");
 
 var port = process.env.PORT || 8000;
-// mongoose.connect(config.database, { useMongoClient: true }); // connect to database
+mongoose.connect(config.database, { useMongoClient: true }); // connect to database
 // app.set('superSecret', config.secret); // secret variable
 
 // // use body parser so we can get info from POST and/or URL parameters
@@ -139,5 +140,6 @@ app.use('/api', apiRoutes);
 
 
 app.listen(port);
+app.options('*', cors()); 
 
 console.log('Healthy Fling server started on: ' + port);
