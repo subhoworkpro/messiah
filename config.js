6b9bba46e690a09cde3334391edc53b930ccf46e
Subhashish Dutta <sdutta@uplandsoftware.com>
2018-12-03 12:52:49 +0530

    Updates

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'admintoken' : 'U2FsdGVkX19v0jsoa/94adkW34+Ro8j/aTnHIBhO6U8=',
     'key' : 'GhPMvymzAN',
-    'secret': 'intellirioconsultanylabs',
+    'secret': 'messiahchariatablefoundationintelliriollp',
     'database': 'mongodb://messiah:India123@localhost:27017/messiah'
 };
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var cors = require("cors");
 
 var port = process.env.PORT || 8000;
 mongoose.connect(config.database, { useMongoClient: true }); // connect to database
-// app.set('superSecret', config.secret); // secret variable
+app.set('superSecret', config.secret); // secret variable
 
 // // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false, limit:'5mb' }));
@@ -36,79 +36,53 @@ console.log(CryptoJS.AES.encrypt('my message', config.key).toString());
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
 // apiRoutes.post('/authenticate', function(req, res) {
 
-// 	// find the user
-// 	User.findOne({
-// 	  name: req.body.name
-// 	}, function(err, user) {
-
-// 	  if (err) throw err;
-
-// 	  if (!user) {
-// 	    res.json({ success: false, message: 'Authentication failed. User not found.' });
-// 	  } else if (user) {
-
-// 	  	var bytes  = CryptoJS.AES.decrypt(user.password, config.key);
-// 		var password = bytes.toString(CryptoJS.enc.Utf8);
-
-// 	    // check if password matches
-// 	    if (password != req.body.password) {
-// 	      res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-// 	    } else {
-
-// 	      // if user is found and password is right
-// 	      // create a token with only our given payload
-// 	  // we don't want to pass in the entire user since that has the password
-// 	  const payload = {
-// 	    admin: user.admin 
+// 	const payload = {
+// 	    admin: "Subhasish Dutta 1988"
 // 	  };
-// 	      var token = jwt.sign(payload, app.get('superSecret'));
+// 	  var token = jwt.sign(payload, app.get('superSecret'));
 
-// 	      // return the information including token as JSON
-// 	      res.json({
-// 	        success: true,
-// 	        message: 'Enjoy your token!',
-// 	        token: token
-// 	      });
-// 	    }   
+// 	  // return the information including token as JSON
+// 	  res.json({
+// 	    success: true,
+// 	    message: 'Enjoy your token!',
+// 	    token: token
+// 	  });  
 
-// 	  }
-
-// 	});
 // });
 
  // route middleware to verify a token
-// apiRoutes.use(function(req, res, next) {
+apiRoutes.use(function(req, res, next) {
 
-//   // check header or url parameters or post parameters for token
+  // check header or url parameters or post parameters for token
 
-//   var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
  
-//   // decode token
-//   if (token) {
+  // decode token
+  if (token) {
 
-//     // verifies secret and checks exp
-//     jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
-//       if (err) {
-//         return res.json({ success: false, message: 'Failed to authenticate token.' });    
-//       } else {
-//         // if everything is good, save to request for use in other routes
-//         req.decoded = decoded;    
-//         next();
-//       }
-//     });
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
 
-//   } else {
+  } else {
 
-//     // if there is no token
-//     // return an error
-//     return res.status(403).send({ 
-//         success: false, 
-//         message: 'No token provided.' 
-//     });
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
 
-//   }
-// });
+  }
+});
 
 app.options('*', cors()); 
 
